c621f3cb761778f618fe9131c016392766b9ff79
esrabedir <esrabdir@gmail.com>
2024-05-23 13:34:57 +0300

    fixed

--- JS/script.js
+++ JS/script.js
@@ -141,10 +141,22 @@ window.onload = function() {
     heading.classList.add('animate');
 };
 
-$("#scrollup").click(function() {
-  $("html, body").animate({ scrollTop: 0 }, "slow");
-  return false;
-});
+let addToButton = document.getElemanById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addaEventListener('click',function(){
+  var paragraph = document.createElement('p')
+  paragraph.classList.add('paragraph-styling')
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value="";
+  paragraph.addEventListener('click',function(){
+  paragraph.style.textDecoration = "line-through";
+  paragraph.addEventListener('dblclick',function(){
+    toDoContainer.removeChild(paragraph);
+  })
+})
 
 const commentForm = document.getElementById('commentForm');
 const commentList = document.getElementById('commentList');
